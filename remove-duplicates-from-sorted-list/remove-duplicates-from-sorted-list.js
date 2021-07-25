a2457d741d262f549ecb593fba7ce3b1145f37cc
Matt Sewell <74661225+matt5342@users.noreply.github.com>
2021-07-25 08:35:29 -0400

    Time: 84 ms (79.74%), Space: 40.7 MB (58.84%) - LeetHub

---
+++ remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    let prev = head
+    if (!head || !head.next) {
+        return head;
+    }
+    let curr = head.next
+    while (curr) {
+        if (curr.val > prev.val) {
+            prev = curr
+            curr = curr.next
+        }
+        else {
+            curr = curr.next
+            prev.next = curr
+        }
+    }
+    return head;
+    
+};
